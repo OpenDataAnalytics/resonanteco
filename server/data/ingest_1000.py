2e7d5d9f05e8a4e0bbbc31ab3d5fd55fbcfd6315
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-08-19 14:27:07 -0400

    Inlude extra metadata to girder items

--- server/data/ingest_1000.py
+++ server/data/ingest_1000.py
@@ -1,16 +1,33 @@
 import csv
 import girder_client
+import requests
 
 gc = girder_client.GirderClient(apiUrl='http://localhost:8080/api/v1')
 gc.authenticate('admin', 'letmein')
 parent = gc.resourceLookup('collection/ResonantEco/datasets/LLNL')
 
+
+def lookup_envo_number(envo):
+    response = requests.get('https://www.ebi.ac.uk/ols/api/select',
+                     params={'q': envo})
+    json_response = response.json()
+    return json_response['response']['docs'][0]['label']
+
+
+
 def create_item_from_row(row):
     print("Ingesting {}".format(row[0]))
     item = gc.createItem(parent['_id'], row[0], reuseExisting=True)
-    metadata = {}
+    metadata = {
+        'latitude': None,
+        'longitude': None,
+        'source': 'JGI'
+    }
     metadata['timestamp'] = row[48]
     metadata['name'] = row[1]
+    metadata['biome'] = lookup_envo_number(row[5])
+    metadata['feature'] = lookup_envo_number(row[6])
+    metadata['material'] = lookup_envo_number(row[7])
     gc.addMetadataToItem(item['_id'], {'meta': metadata})
 
 def create_items_from_csv(path):
