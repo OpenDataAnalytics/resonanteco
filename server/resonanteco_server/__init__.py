98937fbd50b47a4ab4ea9df35c70c1ee45617a05
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-10-07 20:17:42 -0400

    Serve static client from nginx as opposed to girder

--- server/resonanteco_server/__init__.py
+++ server/resonanteco_server/__init__.py
@@ -1,19 +1,12 @@
-import datetime
-from girder import events, plugin
-from girder.models.user import User
-from girder.models.item import Item
+from girder import plugin
 
-from .client_webroot import ClientWebroot
 from .rest.record import Record
 from .rest.meta import Meta
+
 
 class GirderPlugin(plugin.GirderPlugin):
     DISPLAY_NAME = 'ResonantEco server'
 
     def load(self, info):
-        # Relocate Girder
-        info['serverRoot'], info['serverRoot'].girder = (ClientWebroot(),
-                                                         info['serverRoot'])
-        info['serverRoot'].api = info['serverRoot'].girder.api
         info['apiRoot'].record = Record()
         info['apiRoot'].meta = Meta()
